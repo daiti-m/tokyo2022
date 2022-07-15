2e3a659e2e693446968a3ef0d4b69b47c1c4ee21
Daichi Mochihashi <daichi.mochihashi@gmail.com>
2022-07-15 23:47:20 +0900

    Add files via upload

--- bin/areavec-3d.py
+++ bin/areavec-3d.py
@@ -2,8 +2,10 @@
 
 import sys
 import csv
+import putil
 import numpy as np
 import japanize_matplotlib
+from mpl_toolkits.mplot3d import Axes3D
 from scipy.linalg import svd
 from pylab import *
 
@@ -11,8 +13,8 @@ M = 15
 
 def compress (matrix, M):
     U,S,V = svd (matrix[:,0:M])
-    return -np.dot (U[:,0:2], diag(sqrt(S[0:2]))), \
-           -np.dot(V[0:2,:].T, diag(sqrt(S[0:2])))
+    return -np.dot (U[:,0:3], diag(sqrt(S[0:3]))), \
+           -np.dot(V[0:3,:].T, diag(sqrt(S[0:3])))
 
 def load (file):
     matrix = []
@@ -52,6 +54,10 @@ def main ():
     candidates = persons (data + '.dat')
     areavec, candvec = compress (matrix, M)
 
+    fig = figure(figsize=(10,7))
+    ax = fig.gca (projection='3d')
+    putil.simple3d (ax)
+
     #
     #  plot areas
     #
@@ -59,7 +65,7 @@ def main ():
     for n in range(N):
         area = areas[n]
         coord = areavec[n,:]
-        text (-coord[0], coord[1], area, size=10)
+        ax.text (coord[0], coord[1], coord[2], area, size=10)
 
     #
     #  plot candidates
@@ -67,10 +73,17 @@ def main ():
     for n in range(M):
         cand = candidates[n]
         coord = candvec[n,:]
-        text (-coord[0], coord[1], cand, size=10, color='blue')
+        ax.text (coord[0], coord[1], coord[2], cand, size=10, color='blue')
 
-    # axis ([-0.7,1.6,-1,1.2])	# whole
-    axis ([-0.3,0.5,-0.2,0.25])	# land
+    # whole
+#     ax.set_xlim (-0.7,0.7)
+#     ax.set_ylim (-0.7,0.7)
+#     ax.set_zlim (-0.5,0.7)
+    # land
+    ax.set_xlim (-0.3,0.3)
+    ax.set_ylim (-0.3,0.3)
+    ax.set_zlim (-0.3,0.3)
+    
     savefig (output, dpi=200)
     show ()
 
